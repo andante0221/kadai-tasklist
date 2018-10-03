bea6dc5ae22468554eb0a3add9b61d8c5d8abd9d
EC2 Default User <ec2-user@ip-172-31-40-74.ec2.internal>
2018-10-03 14:32:42 +0000

    kadai-commit2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180925134917) do
+ActiveRecord::Schema.define(version: 20180930202759) do
 
   create_table "tasklists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180925134917) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
